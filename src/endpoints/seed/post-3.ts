7fcfe6145903b05374fb7f5982aca2ea43ff3c7f
Quang <quang@Quangs-MacBook-Pro.local>
2026-08-25 15:04:56 +0700

    Update seed post 3 to use the new primaryAuthor relationship

--- src/endpoints/seed/post-3.ts
+++ src/endpoints/seed/post-3.ts
@@ -9,7 +9,7 @@ export const post3: (args: PostArgs) => RequiredDataFromCollectionSlug<'posts'> 
   return {
     slug: 'dollar-and-sense-the-financial-forecast',
     _status: 'published',
-    authors: [author],
+    primaryAuthor: author.id,
     content: {
       root: {
         type: 'root',
